a15cb3c3948525a21d25e2b9056ecaa237366e03
AbbagoR <dementiev.mihail@yandex.ru>
2022-01-20 16:52:00 +0300

    Create HW2.java

---
+++ HW2.java
@@ -0,0 +1,37 @@
+/**
+ * Java 1. Homework 2
+ * 
+ * @author Mikhail
+ * @version 20.01.2022
+ */
+class HW2 {
+    public static void main (String[] args) {
+        System.out.println(checkSum(24, 2));
+        checkNumber(-7);
+        System.out.println(checkPositive(-8));
+        writestring("apple", 3);
+        System.out.println(checkYear(2400));
+    }
+    static boolean checkSum(int a, int b) {
+        boolean c = (a + b > 10 && a + b <= 20);
+        return c;
+    }
+    static void checkNumber(int a) {
+        System.out.println(a >= 0? "Number is positive" : "Number is negative");
+    }
+    static boolean checkPositive(int a) {
+        boolean c = a < 0;
+        return c;
+    }
+    static void writestring(String a, int b) {
+        int w = 1;
+        while (w <= b) {
+            System.out.println(a);
+            w++;
+        }
+    }
+    static boolean checkYear(int a) {
+        boolean c = ((a % 4 == 0) && (a % 100 != 0)) || ((a % 100 == 0) && (a % 400 == 0));
+        return c;
+    }
+}
